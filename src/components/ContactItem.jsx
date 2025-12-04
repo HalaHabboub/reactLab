1227b68e042abb0a1529606691d086e42cb7688b
HalaHabboub <22110029@htu.edu.jo>
2025-12-04 15:06:35 +0300

    contact form

---
+++ src/components/ContactItem.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+// export default function ContactItem({ person }) {
+//     return (
+//         <div>
+//             <li>{person.username} - {person.email}</li>
+//         </div>
+//     )
+// }
